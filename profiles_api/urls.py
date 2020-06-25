d39cf361e2d24876b7dc516d85f78e435584bf88
Jayashri <jayashri1394@gmail.com>
2020-06-25 19:11:04 +0200

    url is added for api views

---
+++ profiles_api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from profiles_api import views
+
+url_patterns = [
+    path('hello-view/',views.HelloApiView.as_view())
+]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API view"""
+    def get(self,request,format=None):
+        """Retrieve a list f API view features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get,post,patch,put,delete)',
+            'My first APIVIEW',
+            'Information from the list present in APIVIEW'
+        ]
+
+        return Response({'message':'Hello!','an_apiview':an_apiview})
+
